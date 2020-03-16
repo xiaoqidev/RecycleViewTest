433e840591b70450f02eb01657a12e13eff12c29
xiaoqi <491298880@qq.com>
2020-03-16 22:00:30 +0800

    q

--- app/src/main/java/com/example/recycleviewtest/FruitAdapter.java
+++ app/src/main/java/com/example/recycleviewtest/FruitAdapter.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
     private List<Fruit> mFruitList;
 
-    @NonNull
+    @NonNull//
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item,parent,false);
@@ -57,14 +57,14 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
         ImageView fruitImage;
         TextView fruitName;
 
-        public ViewHolder(View view){
+        ViewHolder(View view){
             super(view);
             fruitView = view;
             fruitImage = view.findViewById(R.id.fruit_image);
             fruitName = view.findViewById(R.id.fruit_name);
         }
     }
-    public FruitAdapter(List<Fruit> fruitList){
+    FruitAdapter(List<Fruit> fruitList){
         mFruitList = fruitList;
     }
 }
